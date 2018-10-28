628ab96fc20401d529e130ba5ce2de5ca9a6442b
MARCELO FRANCISCONI LUBANCO THOMÉ <lubanco@edu.unifil.br>
2018-10-28 00:29:09 -0300

    [LOGISTICA][SERVICE] Separação de métodos e exceptions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,12 +9,10 @@ import java.util.List;
 
 public class Main {
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws Exception {
 
     Service service = new Service();
     ArrayList<Frete> fretes =  new ArrayList<>();
-    boolean isCombinado =  true;
-    int resultado[][] = new int[50][50];
     ArrayList<Cliente> clientes = new ArrayList<Cliente>();
     final ArrayList<Caminhao> caminhaos = new ArrayList<Caminhao>();
 //    String nome = JOptionPane.showInputDialog("Informe o caminho do arquivo");
@@ -25,9 +23,8 @@ public class Main {
     String linha = lerArq.readLine();
     while (linha != null) {
       lista.add(linha);
-      linha = lerArq.readLine(); // lê da segunda até a última linha
+      linha = lerArq.readLine();
     }
-
 
     String comeco[] = lista.get(0).split(" ");
     int finalCliente = Integer.parseInt(comeco[0]) + 1;
@@ -36,43 +33,49 @@ public class Main {
     int finalCaminhao = finalCliente + Integer.parseInt(comeco[1]);
 
 
-    for (int i = 1; i < finalCliente; i++) {
-      String dados = lista.get(i);
-      String dadosSeparados[] = dados.split(" ");
-      clientes.add(new Cliente(Long.valueOf(dadosSeparados[0]), Long.valueOf(dadosSeparados[1])));
+    try {
+      for (int i = 1; i < finalCliente; i++) {
+        String dados = lista.get(i);
+        String dadosSeparados[] = dados.split(" ");
+        clientes.add(new Cliente(Long.valueOf(dadosSeparados[0]), Long.valueOf(dadosSeparados[1])));
+      }
+    } catch (Exception e) {
+      throw new Exception("Os dados de clientes não são compativeis");
     }
 
-
-    for (int i = finalCliente; i < finalCaminhao; i++) {
-      String dados = lista.get(i);
-      String dadosSeparados[] = dados.split(" ");
-      caminhaos.add(new Caminhao(Long.valueOf(dadosSeparados[0]), Long.valueOf(dadosSeparados[1])));
+    try {
+      for (int i = finalCliente; i < finalCaminhao; i++) {
+        String dados = lista.get(i);
+        String dadosSeparados[] = dados.split(" ");
+        caminhaos.add(new Caminhao(Long.valueOf(dadosSeparados[0]), Long.valueOf(dadosSeparados[1])));
+      }
+    } catch(Exception e ) {
+      throw new Exception("Os dados de caminhões não são compativeis");
     }
 
     Collections.sort(caminhaos);
 
     Collections.sort(clientes);
-    final ArrayList<Integer> combinacoes = new ArrayList<Integer>();
 
-
-      for(int i=0; i < caminhaos.size();i++) {
-        for(int j=0; j < clientes.size(); j++) {
-        if(clientes.get(j).getQuantidade() <= caminhaos.get(i).getQuantidade()
-                && clientes.get(j).getValor() >= caminhaos.get(i).getValor()) {
-          if(isCombinado) {
-            fretes.add(new Frete(clientes.get(i), caminhaos.get(i)));
-            combinacoes.add(1);
-            isCombinado = false;
-          }
-        } else {
-          combinacoes.add(0);
-        }
-      }
-      isCombinado = true;
-    }
-
+    fretes = service.combinacoesFrete(caminhaos,clientes);
     service.gravarResultadoArquivo(fretes.size());
-    service.listarCombinacoes(combinacoes);
+//      for(int i=0; i < caminhaos.size();i++) {
+//        for(int j=0; j < clientes.size(); j++) {
+//        if(clientes.get(j).getQuantidade() <= caminhaos.get(i).getQuantidade()
+//                && clientes.get(j).getValor() >= caminhaos.get(i).getValor()) {
+//          if(isCombinado) {
+//            fretes.add(new Frete(clientes.get(i), caminhaos.get(i)));
+//            combinacoes.add(1);
+//            isCombinado = false;
+//          }
+//        } else {
+//          combinacoes.add(0);
+//        }
+//      }
+//      isCombinado = true;
+//    }
+
+
   }
 
 
--- src/main/java/Service.java
+++ src/main/java/Service.java
@@ -1,3 +1,7 @@
+import model.Caminhao;
+import model.Cliente;
+import model.Frete;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -14,6 +18,25 @@ public class Service {
     PrintWriter gravarArq = new PrintWriter(arquivo);
     gravarArq.print(valorResultado);
     arquivo.close();
+  }
+
+  public ArrayList<Frete> combinacoesFrete(ArrayList<Caminhao> caminhaos, ArrayList<Cliente> clientes) {
+    boolean isCombinado =  true;
+    ArrayList<Frete> fretes =  new ArrayList<>();
+    for(int i=0; i < caminhaos.size();i++) {
+      for(int j=0; j < clientes.size(); j++) {
+        if(clientes.get(j).getQuantidade() <= caminhaos.get(i).getQuantidade()
+                && clientes.get(j).getValor() >= caminhaos.get(i).getValor()) {
+          if(isCombinado) {
+            fretes.add(new Frete(clientes.get(i), caminhaos.get(i)));
+            isCombinado = false;
+          }
+        } else {
+        }
+      }
+      isCombinado = true;
+    }
+    return fretes;
   }
 
 }
